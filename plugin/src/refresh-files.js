eb7bb0b2989de5ad9253d96162fc484e513ee610
Mathieu Dutour <mat.dutour@gmail.com>
2017-07-06 22:33:55 +0200

    add refresh-files plugin command

---
+++ plugin/src/refresh-files.js
@@ -0,0 +1,16 @@
+export default function (context) {
+  const documents = NSApplication.sharedApplication().orderedDocuments()
+
+  for (let i = 0; i < documents.length; i++) {
+    const document = documents[i]
+
+    // reload all the files that doesn't unsaved changes
+    if (!document.isDocumentEdited()) {
+      document.readFromURL_ofType_error(
+        document.fileURL(),
+        document.fileType(),
+        null
+      )
+    }
+  }
+}
