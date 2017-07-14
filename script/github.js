df485f188f92d1728c7b3e8efa5e8fd2182e3eae
Mathieu Dutour <mat.dutour@gmail.com>
2017-07-14 23:24:58 +0100

    prettify

--- script/github.js
+++ script/github.js
@@ -2,7 +2,7 @@ const path = require('path')
 const fs = require('fs')
 const _request = require('request')
 
-function getErrorFromBody (body, opts) {
+function getErrorFromBody(body, opts) {
   if (typeof body === 'string') {
     try {
       body = JSON.parse(body)
@@ -15,9 +15,9 @@ function getErrorFromBody (body, opts) {
   return new Error(JSON.stringify(body, null, '  '))
 }
 
-function request (opts) {
-  return new Promise(function (resolve, reject) {
-    _request(opts, function (err, response, body) {
+function request(opts) {
+  return new Promise(function(resolve, reject) {
+    _request(opts, function(err, response, body) {
       if (err) {
         return reject(err)
       }
@@ -30,39 +30,55 @@ function request (opts) {
   })
 }
 
-function options (token, url, method) {
+function options(token, url, method) {
   return {
     method: method || 'GET',
     url: url,
     headers: {
-      'Accept': 'application/vnd.github.v3+json',
-      'Authorization': 'Token ' + token,
-      'User-Agent': 'SKPM-Release-Agent'
-    }
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: 'Token ' + token,
+      'User-Agent': 'SKPM-Release-Agent',
+    },
   }
 }
 
 module.exports = {
-  getOrCreateDraftRelease: function (token, repo, tag, branch, changelog) {
-    return request(options(token, 'https://api.github.com/repos/' + repo + '/releases/tags/' + tag)).then(function (res) {
-      return res
-    }).catch(function () {
-      var opts = options(token, 'https://api.github.com/repos/' + repo + '/releases', 'POST')
-      opts.json = {
-        tag_name: tag,
-        target_commitish: branch,
-        body: '* ' + changelog.join('\n* '),
-        name: tag,
-        draft: true
-      }
-      return request(opts)
-    })
+  getOrCreateDraftRelease: function(token, repo, tag, branch, changelog) {
+    return request(
+      options(
+        token,
+        'https://api.github.com/repos/' + repo + '/releases/tags/' + tag
+      )
+    )
+      .then(function(res) {
+        return res
+      })
+      .catch(function() {
+        var opts = options(
+          token,
+          'https://api.github.com/repos/' + repo + '/releases',
+          'POST'
+        )
+        opts.json = {
+          tag_name: tag,
+          target_commitish: branch,
+          body: '* ' + changelog.join('\n* '),
+          name: tag,
+          draft: true,
+        }
+        return request(opts)
+      })
   },
-  updateAsset: function (token, repo, releaseId, asset, fileName) {
+  updateAsset: function(token, repo, releaseId, asset, fileName) {
     const name = path.basename(asset)
     var opts = options(
       token,
-      'https://uploads.github.com/repos/' + repo + '/releases/' + releaseId + '/assets?name=' + encodeURIComponent(name),
+      'https://uploads.github.com/repos/' +
+        repo +
+        '/releases/' +
+        releaseId +
+        '/assets?name=' +
+        encodeURIComponent(name),
       'POST'
     )
     var stat = fs.statSync(asset)
@@ -71,25 +87,29 @@ module.exports = {
     opts.headers['Content-Length'] = stat.size
     var us = _request(opts)
 
-    return new Promise(function (resolve, reject) {
-      rd.on('error', function (err) {
+    return new Promise(function(resolve, reject) {
+      rd.on('error', function(err) {
         return reject(err)
       })
-      us.on('error', function (err) {
+      us.on('error', function(err) {
         return reject(err)
       })
-      us.on('end', function () {
-        resolve({ name: name });
+      us.on('end', function() {
+        resolve({ name: name })
       })
 
       rd.pipe(us)
     })
   },
-  publishRelease: function (token, repo, releaseId) {
-    var opts = options(token, 'https://api.github.com/repos/' + repo + '/releases/' + releaseId, 'PATCH')
+  publishRelease: function(token, repo, releaseId) {
+    var opts = options(
+      token,
+      'https://api.github.com/repos/' + repo + '/releases/' + releaseId,
+      'PATCH'
+    )
     opts.json = {
-      draft: false
+      draft: false,
     }
     return request(opts)
-  }
+  },
 }
